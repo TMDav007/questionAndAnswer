d29b3b57dd50757e86133fb3f7b9442a8bc85fd1
Afolabi <opeyemi.afolabi@ubagroup.com>
2020-02-21 09:37:40 +0100

    add animated sidebar for mobile screen

--- UI/script.js
+++ UI/script.js
@@ -1,25 +1,32 @@
-let dropDown = document.querySelector("nav .dropdown");
+let dropDown = document.querySelector("nav .dropdown") 
+let sideBar =  document.querySelector(".sidebar");
 let menuBar = document.querySelector("nav .menu");
 
 const menu = () => {
     if (dropDown.style.display === "grid"){
-        dropDown.style.display = "none";
-        menuBar.innerHTML = "menu";
+      dropDown.style.display = "none";
+      menuBar.innerHTML = "menu";
     }
-        else {
-            dropDown.style.display = "grid";
-            
-            menuBar.innerHTML = "close";
-        }
+    else {
+        dropDown.style.display = "grid";  
+        menuBar.innerHTML = "close";
     }
+  }
 
+const toggle = () => {
+   if (sideBar.classList.contains("active")) {
+     sideBar.classList.remove('active');
+     menuBar.innerHTML = "menu";
+   }
+   else {
+     sideBar.classList.add('active');
+     menuBar.innerHTML = "close";
+   }
+}
 
-menuBar.addEventListener("click", ()=> {
-        menu();
-    });
 addEventListener("resize", ()=> {
     if (window.innerWidth > 700) {
         dropDown.style.display = "none";
-        menuBar.innerHTML = "menu"
+        menuBar.innerHTML = "menu";
     }
 });
