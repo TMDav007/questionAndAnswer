f0a33410f1ead3471ed988b1b0ff32eb52c601b1
Afolabi <opeyemi.afolabi@ubagroup.com>
2020-04-01 19:35:17 +0100

    add a dropdown menu to users page

--- UI/script.js
+++ UI/script.js
@@ -1,6 +1,7 @@
 let dropDown = document.querySelector("nav .dropdown") 
 let sideBar =  document.querySelector(".sidebar");
 let menuBar = document.querySelector("nav .menu");
+let dropDownMenu = document.querySelector(".menu_list");
 
 const menu = () => {
     if (dropDown.style.display === "grid") {
@@ -12,6 +13,18 @@ const menu = () => {
         menuBar.innerHTML = "close";
     }
   }
+
+const dropDownMenuList = () => {
+  //console.log("im here");
+  if (dropDownMenu.style.display === "block") {
+    console.log("im here");
+    dropDownMenu.style.display = "none";
+  } 
+  else {
+    dropDownMenu.style.display = "block";
+  }
+
+}
 
 const toggle = () => {
    if (sideBar.classList.contains("active")) {
