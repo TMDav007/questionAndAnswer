130b40ca109980c1a68452834ea1415a8eda16a9
Afolabi <opeyemi.afolabi@ubagroup.com>
2020-07-23 07:09:03 +0100

    - I add navbar, hompage and footer components - I add style with scss - I add asset folder to store images - I modified package.json

---
+++ client/components/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class footer extends Component {
+  render() {
+    return (
+      <div id="footer" className="grid">
+         <div className="content-wrap"></div>
+      </div>
+    )
+  }
+}
+
+export default footer
---
+++ client/components/homepage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class homepage extends Component {
+  render() {
+    return (
+      <div id="showcase" className="grid">
+          <div id="bg-image"></div>
+          <div className="content-wrap words">
+            <div id="question_answer">
+                <h1>Question&Answer</h1>
+                <p>Get answers to your questions</p>
+                <div id="get_started">
+                    <a href="signup.html">Get Started</a>
+                    <a href="login.html">Login</a>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default homepage
