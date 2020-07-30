d3a535af2fa97205ad396fab2a24893064c7adb9
Afolabi <opeyemi.afolabi@ubagroup.com>
2020-07-30 17:36:00 +0100

    - I add the createuser method - I modify the server.js to enable CORS

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes/routes'
+import cors from 'cors';
+import routes from './routes/routes';
+import path from 'path';
 import babelPolyfill from 'babel-polyfill';
 
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json({ type: 'application/json'}));
 
 routes(app);
+
+app.use('/public',express.static(path.join(__dirname,'../client/public')));
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
