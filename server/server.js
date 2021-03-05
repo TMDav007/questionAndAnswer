23697891a8c8d3e29cf2cd4874d75559f1c548e1
Michael Tolu Davies <afolabi.toluwalase@yahoo.com>
2021-03-05 01:20:03 +0000

    - upload static pages on heroku (#39)

--- server/server.js
+++ server/server.js
@@ -14,12 +14,10 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json({ type: 'application/json'}));
 
-routes(app);
-
 if (process.env.NODE_ENV !== 'development' || process.env.NODE_ENV !== 'test') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 }
 
